64f84bb572142f32a640b6439d51235f44590c99
tfgGG <jessicahuangtfg@gmail.com>
2017-05-09 13:32:45 +0800

    part2

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,15 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            /*routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movie/released/{year}/{month}",
+                new { controller = "Movie", action = "ByReleaseDate" },
+                new { year = "\\d{4}", month =@"\d{2}" );
+            */
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,42 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            var customer = new List<Customer>
+            {
+                new Customer {Name="customer1" },
+                new Customer { Name="customer2"}
+            };
+            
+            var vieModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customer = customer
+            };
+
+            return  View(vieModel);
+            // ViewData["Movie"] = movie;
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model.
+        }
+        
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id" + movieId);
+        } 
+
+        public ActionResult Index(int ? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+
+       // [Route("movies/realeased/{year}/{month:regex(\\d{4}):range(1-4)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year + "/"+month);
         }
     }
 }
